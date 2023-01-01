1b92831b758b809d416cf9d153c49d77ef5ade09
Dimitar <wopz1337@gmail.com>
2023-01-01 22:04:15 +0200

    Added Firebase Config File

---
+++ src/configs/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBl9AxZyONg11vFSrV0_aqh1ag1UtaPC2Y",
+  authDomain: "realtor-clone-55711.firebaseapp.com",
+  projectId: "realtor-clone-55711",
+  storageBucket: "realtor-clone-55711.appspot.com",
+  messagingSenderId: "106413853323",
+  appId: "1:106413853323:web:30da4c4255252655fca28e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
