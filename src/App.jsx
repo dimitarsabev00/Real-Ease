20ba3003723ccfe000a8f4b5c48b16ef3554aeb3
Dimitar <wopz1337@gmail.com>
2023-01-02 13:09:51 +0200

    Added Notification Container

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <>
@@ -23,6 +24,18 @@ function App() {
           <Route path="*" element={<ErrorPage />} />
         </Routes>
       </Router>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        // theme="dark"
+      />
     </>
   );
 }
