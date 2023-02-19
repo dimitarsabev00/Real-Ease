ad60850c9b751b557c8f4b61a6b8771d2631f595
Dimitar Sabev <dimitarsabev00@gmail.com>
2023-02-19 10:54:15 +0200

    Finish Design For Profile Page & Added Private Route Functionality

---
+++ src/hooks/useAuthStatus.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../configs/firebase";
+
+const useAuthStatus = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [checkingStatus, setCheckingStatus] = useState(true);
+
+  useEffect(() => {
+    console.log(auth);
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setLoggedIn(true);
+      }
+      setCheckingStatus(false);
+    });
+  }, []);
+  return { loggedIn, checkingStatus };
+};
+
+export default useAuthStatus;
