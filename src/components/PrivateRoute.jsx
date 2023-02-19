e3c63201ec2e63262ae4b6ea099e45989c888865
Dimitar Sabev <dimitarsabev00@gmail.com>
2023-02-19 11:32:38 +0200

    Added Loading Component

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,10 +1,11 @@
 import { Outlet, Navigate } from "react-router-dom";
 import useAuthStatus from "../hooks/useAuthStatus";
+import Loading from "./Loading";
 
 const PrivateRoute = () => {
   const { loggedIn, checkingStatus } = useAuthStatus();
   if (checkingStatus) {
-    return <h3>Loading...</h3>;
+    return <Loading />;
   }
   return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
 };
