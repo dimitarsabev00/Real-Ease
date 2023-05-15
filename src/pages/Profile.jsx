bf18692511919886821306e06939d73c54a5c1a4
Dimitar Sabev <dimitarsabev00@gmail.com>
2023-05-15 08:41:57 +0300

    Added Styling For Container With My Listings In Profile Page Component

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { auth, db } from "../configs/firebase";
-
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { updateProfile } from "firebase/auth";
@@ -145,14 +144,12 @@ const Profile = () => {
       <div className="max-w-6xl px-3 mt-6 mx-auto">
         {!loading && listings.length > 0 && (
           <>
-            <h2 className="text-2xl text-center font-semibold">My Listings</h2>
-            <ul>
-              {listings.map((listing) => (
-                <ListingItem
-                  key={listing.id}
-                  id={listing.id}
-                  listing={listing.data}
-                />
+            <h2 className="text-2xl text-center font-semibold mb-6">
+              My Listings
+            </h2>
+            <ul className="sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+              {listings.map(({ data, id }) => (
+                <ListingItem key={id} id={id} listing={data} />
               ))}
             </ul>
           </>
