ec91cda57cf276db1505631a90dab8eed710b140
Dimitar Sabev <dimitarsabev00@gmail.com>
2023-05-15 08:08:27 +0300

    Fetch & Render Listing Items In Profile Component

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,12 +4,24 @@ import { auth, db } from "../configs/firebase";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { updateProfile } from "firebase/auth";
-import { doc, updateDoc } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDocs,
+  orderBy,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 import { toast } from "react-toastify";
 import { FcHome } from "react-icons/fc";
+import { useEffect } from "react";
+import ListingItem from "../components/ListingItem";
 const Profile = () => {
   const navigate = useNavigate();
   const [changeDetail, setChangeDetail] = useState(false);
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
@@ -45,6 +57,27 @@ const Profile = () => {
       toast.error("Could not update the profile details");
     }
   }
+  const fetchUserListings = async () => {
+    const listingRef = collection(db, "listings");
+    const q = query(
+      listingRef,
+      where("userRef", "==", auth.currentUser.uid),
+      orderBy("timestamp", "desc")
+    );
+    const querySnap = await getDocs(q);
+    let listings = [];
+    querySnap.forEach((doc) => {
+      return listings.push({
+        id: doc.id,
+        data: doc.data(),
+      });
+    });
+    setListings(listings);
+    setLoading(false);
+  };
+  useEffect(() => {
+    fetchUserListings();
+  }, [auth.currentUser.uid]);
   return (
     <>
       <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
@@ -109,6 +142,22 @@ const Profile = () => {
           </button>
         </div>
       </section>
+      <div className="max-w-6xl px-3 mt-6 mx-auto">
+        {!loading && listings.length > 0 && (
+          <>
+            <h2 className="text-2xl text-center font-semibold">My Listings</h2>
+            <ul>
+              {listings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  id={listing.id}
+                  listing={listing.data}
+                />
+              ))}
+            </ul>
+          </>
+        )}
+      </div>
     </>
   );
 };
